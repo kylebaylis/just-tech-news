a8746edd1587172052746055e7bfd29f8ede0bea
Kyle <kylejmbaylis@gmail.com>
2022-05-19 19:10:17 -0400

    added upvotes

---
+++ models/Vote.js
@@ -0,0 +1,41 @@
+// This is a many-to-many model
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model {}
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // user_id and post_id are to connect User and Post models
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+module.exports = Vote;
