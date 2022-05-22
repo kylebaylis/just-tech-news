9f0048d38a2fcc3d5c0a4523bc1fc64d03da64a0
Kyle <kylejmbaylis@gmail.com>
2022-05-22 17:42:02 -0400

    homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
 
 router.use('/comments', commentRoutes);
+
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
