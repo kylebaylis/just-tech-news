01774dae34596b9bd2a73179901f7f178c949ba1
Kyle <kylejmbaylis@gmail.com>
2022-05-23 14:50:42 -0400

    login/logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,7 +64,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -84,7 +92,6 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'No user with that email address' });
             return;
         }
-        // res.json({ user: dbUserData }); commented out as part of the lesson
 
         // Verify user
         const validPassword = dbUserData.checkPassword(req.body.password);
@@ -92,7 +99,14 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect password' });
             return;
         }
-        res.json({ user: dbUserData, message: 'You are now logged in' });
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in' });
+        });
     });
 });
 
@@ -137,6 +151,18 @@ router.delete('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+// logout
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end;
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -18,6 +31,8 @@ app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
+
+app.use(session(sess));
 
 // turn on connection to db and server
 // force: true means the database connection must sync with the model definitions and associations
